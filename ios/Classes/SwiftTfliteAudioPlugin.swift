414b0bc0fbb12548039fc571f9491a341365676a
Caldarie <michael_nguyen4@hotmail.com>
2020-08-28 17:34:48 +0900

    Set the foundation for swift

--- ios/Classes/SwiftTfliteAudioPlugin.swift
+++ ios/Classes/SwiftTfliteAudioPlugin.swift
@@ -2,13 +2,48 @@ import Flutter
 import UIKit
 
 public class SwiftTfliteAudioPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "tflite_audio", binaryMessenger: registrar.messenger())
-    let instance = SwiftTfliteAudioPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
+    
+    let registrar: FlutterPluginRegistrar;
+    var result: FlutterResult!
+         
+    init(_ _registrar: FlutterPluginRegistrar) {
+        registrar = _registrar  
+    }
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "tflite_audio", binaryMessenger: registrar.messenger())
+        let instance = SwiftTfliteAudioPlugin(registrar)
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+  
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        let arguments = call.arguments as! [String: AnyObject]
+        self.result = result;
+
+        switch call.method{
+        case "loadModel":
+            loadModel(registrar: registrar, args: arguments)
+            break 
+        case "startRecognition":
+            print("button")
+            break
+        default: result(FlutterMethodNotImplemented)
+        }
+    }
+    
+    func loadModel(registrar: FlutterPluginRegistrar, args:[String:AnyObject]){
+      //dump(args)
+      var key: String;
+      let isAsset = args["isAsset"] as! Bool;
+      let model = args["model"] as! String;
+      if(isAsset){
+      key = registrar.lookupKey(forAsset: model);
+      }
+      result("success")
+    }
+
+    func startRecognition(){
+      result("success");
+    }
 }
