1edbde8395de94df72a060a6450cc10a32c51062
Caldarie <michael_nguyen4@hotmail.com>
2020-10-31 22:53:35 +0900

    Added stream support for iOS

--- ios/Classes/SwiftTfliteAudioPlugin.swift
+++ ios/Classes/SwiftTfliteAudioPlugin.swift
@@ -17,13 +17,8 @@ struct Result: Codable {
 public class SwiftTfliteAudioPlugin: NSObject, FlutterPlugin, FlutterStreamHandler {
     
     
-    // public static var event: TfliteAudioEvent?
-    
-    //Streamhandler
-    // private let recognitionStreamHandler = RecognitionStreamHandler();
-    
     //placeholder variables
-    private var eventSink: FlutterEventSink?
+    private var events: FlutterEventSink!
     private var registrar: FlutterPluginRegistrar!
     private var result: FlutterResult!
     private var arguments: [String: AnyObject]!
@@ -57,7 +52,7 @@ public class SwiftTfliteAudioPlugin: NSObject, FlutterPlugin, FlutterStreamHandl
     
     public static func register(with registrar: FlutterPluginRegistrar) {
         let instance = SwiftTfliteAudioPlugin(registrar: registrar)
-
+        
         // MethodChannel
         let channel = FlutterMethodChannel(name: "tflite_audio", binaryMessenger: registrar.messenger())
         registrar.addMethodCallDelegate(instance, channel: channel)
@@ -84,15 +79,228 @@ public class SwiftTfliteAudioPlugin: NSObject, FlutterPlugin, FlutterStreamHandl
         default: result(FlutterMethodNotImplemented)
         }
     }
-
-        
+    
+    
     public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
-        events(arguments)
+        self.events = events
+        self.arguments = arguments as? [String: AnyObject]
+        checkPermissions()
         return nil
     }
     
     public func onCancel(withArguments arguments: Any?) -> FlutterError? {
+        self.events = nil
         return nil
+    }
+    
+    func checkPermissions() {
+        switch AVAudioSession.sharedInstance().recordPermission {
+        case .granted:
+            print("Permission granted")
+            startMicrophone()
+        case .denied:
+            showAlert(title: "Microphone Permissions", message: "Permission denied. Please accept permission in your settings.")
+            let finalResults = Result(recognitionResult: nil, inferenceTime: 0, hasPermission: false)
+            let dict = finalResults.dictionary
+            events(dict!)
+        case .undetermined:
+            print("requesting permission")
+            requestPermissions()
+        @unknown default:
+            print("Something weird just happened")
+        }
+    }
+    
+    func requestPermissions() {
+        AVAudioSession.sharedInstance().requestRecordPermission { (granted) in
+            if granted {
+                
+                self.startMicrophone()
+            }
+            else {
+                print("check permissions")
+                self.checkPermissions()
+            }
+        }
+    }
+    
+    func showAlert(title: String, message: String) {
+        
+        DispatchQueue.main.async {
+            let alertController = UIAlertController(title: title, message:
+                                                        message, preferredStyle: .alert)
+            let rootViewController = UIApplication.shared.keyWindow?.rootViewController
+            let settingsAction = UIAlertAction(title: "Settings", style: .default) { (_) -> Void in
+                guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else {
+                    return
+                }
+                if UIApplication.shared.canOpenURL(settingsUrl) {
+                    UIApplication.shared.open(settingsUrl, completionHandler: { (success) in })
+                }
+            }
+            let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+            alertController.addAction(cancelAction)
+            alertController.addAction(settingsAction)
+            rootViewController?.present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+    
+    func startMicrophone(){
+        print("start microphone")
+        
+        let bufferSize = arguments["bufferSize"] as! Int
+        let sampleRate = arguments["sampleRate"] as! Int
+        let recordingLength = arguments["recordingLength"] as! Int
+        let numOfRecordings = arguments["numOfRecordings"] as! Int
+        
+        let maxRecordingLength = recordingLength * numOfRecordings
+        let recordingFrameBuffer = bufferSize/2 
+        
+        //recording frames are appended and stored in this array
+        var recordingBuffer: [Int16] = [] 
+        var preRecordingCount: Int = 0
+        var recordingCount: Int = recordingLength
+        
+        let inputNode = audioEngine.inputNode
+        let inputFormat = inputNode.outputFormat(forBus: 0)
+        let recordingFormat = AVAudioFormat(commonFormat: .pcmFormatInt16, sampleRate: Double(sampleRate), channels: 1, interleaved: true)
+        guard let formatConverter =  AVAudioConverter(from:inputFormat, to: recordingFormat!) else {
+            return
+        }
+        
+        // install a tap on the audio engine and loops the frames into recordingBuffer
+        audioEngine.inputNode.installTap(onBus: 0, bufferSize: AVAudioFrameCount(bufferSize), format: inputFormat) { (buffer, time) in
+            
+            self.conversionQueue.async {
+                
+                //let pcmBuffer = AVAudioPCMBuffer(pcmFormat: recordingFormat!, frameCapacity: AVAudioFrameCount(recordingFormat!.sampleRate * 2.0))
+                let pcmBuffer = AVAudioPCMBuffer(pcmFormat: recordingFormat!, frameCapacity: AVAudioFrameCount(recordingFrameBuffer))
+                var error: NSError? = nil
+                
+                let inputBlock: AVAudioConverterInputBlock = {inNumPackets, outStatus in
+                    outStatus.pointee = AVAudioConverterInputStatus.haveData
+                    return buffer
+                }
+                
+                formatConverter.convert(to: pcmBuffer!, error: &error, withInputFrom: inputBlock)
+                
+                if error != nil {
+                    print(error!.localizedDescription)
+                }
+                else if let channelData = pcmBuffer!.int16ChannelData {
+                    
+                    let channelDataValue = channelData.pointee
+                    let channelDataValueArray = stride(from: 0,
+                                                       to: Int(pcmBuffer!.frameLength),
+                                                       by: buffer.stride).map{ channelDataValue[$0] }
+                    
+                    recordingBuffer.append(contentsOf: channelDataValueArray)
+                    print("recordingBuffer length: \(recordingBuffer.count)")
+                    
+                    
+                    if(recordingBuffer.count >= recordingCount){
+                        print("reached threshold")
+                        self.runModel(onBuffer: Array(recordingBuffer[preRecordingCount..<recordingCount]))
+                        if(recordingBuffer.count >= maxRecordingLength){
+                            print("Recording stopped.")
+                            // Closes stream
+                            self.events(FlutterEndOfEventStream)
+                            // Stop the recording
+                            self.audioEngine.stop()
+                            inputNode.removeTap(onBus: 0)
+                        }else{
+                            print("Looping...")
+                            recordingCount += recordingLength
+                            preRecordingCount += recordingLength
+                            print(recordingCount)
+                            print(preRecordingCount)
+                        }
+                    }
+                    
+                } //channeldata
+            } //conversion queue
+        } //installtap
+        
+        audioEngine.prepare()
+        do {
+            try audioEngine.start()
+        }
+        catch {
+            print(error.localizedDescription)
+        }
+        
+        
+        
+    }
+    
+    
+    // func runModel(onBuffer buffer: [Int16]) -> Result? {
+    func runModel(onBuffer buffer: [Int16]){
+        print("Running model")
+        let sampleRate = arguments["sampleRate"] as! Int
+        var interval: TimeInterval!
+        var outputTensor: Tensor!
+        
+        do {
+            // Copy the `[Int16]` buffer data as an array of `Float`s to the audio buffer input `Tensor`'s.
+            let audioBufferData = Data(copyingBufferOf: buffer.map { Float($0) / maxInt16AsFloat32 })
+            try interpreter.copy(audioBufferData, toInputAt: audioBufferInputTensorIndex)
+            
+            // Copy the sample rate data to the sample rate input `Tensor`.
+            var rate = Int32(sampleRate)
+            let sampleRateData = Data(bytes: &rate, count: MemoryLayout.size(ofValue: rate))
+            try interpreter.copy(sampleRateData, toInputAt: sampleRateInputTensorIndex)
+            
+            // Calculate inference time
+            let startDate = Date()
+            try interpreter.invoke() //required!!! Do not touch
+            interval = Date().timeIntervalSince(startDate) * 1000
+            
+            // Get the output `Tensor` to process the inference results.
+            outputTensor = try interpreter.output(at: 0)
+            
+            
+            
+        } catch let error {
+            print("Failed to invoke the interpreter with error: \(error.localizedDescription)")
+        }
+        
+        // Gets the formatted and averaged results.
+        let scores = [Float32](unsafeData: outputTensor.data) ?? []
+        let results = getResults(withScores: scores)
+        let roundInterval = interval.rounded();
+        let finalResults = Result(recognitionResult: results, inferenceTime: roundInterval, hasPermission: true)
+        
+        //Convert results to dictionary and then json
+        let dict = finalResults.dictionary
+        print("results: \(dict!)")
+        events(dict!)        
+    }
+    
+    // private func getResults(withScores scores: [Float]) -> RecognitionResult? {
+    private func getResults(withScores scores: [Float]) -> String? {
+        
+        var results: [Float] = []
+        for i in 0..<labelArray.count {
+            results.append(scores[i])
+        }
+        
+        // Runs results through recognize commands.
+        let command = recognitionResult?.process(
+            latestResults: results,
+            currentTime: Date().timeIntervalSince1970 * 1000
+        )
+        
+        //Check if command is new and the identified result is not silence or unknown.
+        // guard let newCommand = command,
+        //   let index = labelArray.firstIndex(of: newCommand.name),
+        //   newCommand.isNew,
+        //   index >= labelOffset
+        // else {
+        //     return nil
+        // }
+        return command?.name
     }
     
     func loadModel(registrar: FlutterPluginRegistrar){
@@ -156,7 +364,62 @@ public class SwiftTfliteAudioPlugin: NSObject, FlutterPlugin, FlutterStreamHandl
         print(labelArray)
     }
     
-
+    
 }
 
 
+//----------------EXTENSIONS-----------
+
+//Used in runModel()
+extension Data {
+    /// Creates a new buffer by copying the buffer pointer of the given array.
+    ///
+    /// - Warning: The given array's element type `T` must be trivial in that it can be copied bit
+    ///     for bit with no indirection or reference-counting operations; otherwise, reinterpreting
+    ///     data from the resulting buffer has undefined behavior.
+    /// - Parameter array: An array with elements of type `T`.
+    init<T>(copyingBufferOf array: [T]) {
+        self = array.withUnsafeBufferPointer(Data.init)
+    }
+}
+
+//Used for startMicrophone()
+extension Array {
+    /// Creates a new array from the bytes of the given unsafe data.
+    ///
+    /// - Warning: The array's `Element` type must be trivial in that it can be copied bit for bit
+    ///     with no indirection or reference-counting operations; otherwise, copying the raw bytes in
+    ///     the `unsafeData`'s buffer to a new array returns an unsafe copy.
+    /// - Note: Returns `nil` if `unsafeData.count` is not a multiple of
+    ///     `MemoryLayout<Element>.stride`.
+    /// - Parameter unsafeData: The data containing the bytes to turn into an array.
+    init?(unsafeData: Data) {
+        guard unsafeData.count % MemoryLayout<Element>.stride == 0 else { return nil }
+        #if swift(>=5.0)
+        self = unsafeData.withUnsafeBytes { .init($0.bindMemory(to: Element.self)) }
+        #else
+        self = unsafeData.withUnsafeBytes {
+            .init(UnsafeBufferPointer<Element>(
+                start: $0,
+                count: unsafeData.count / MemoryLayout<Element>.stride
+            ))
+        }
+        #endif  // swift(>=5.0)
+    }
+}
+
+// Used to encode the struct class Result to json
+extension Encodable {
+    var dictionary: [String: Any]? {
+        guard let data = try? JSONEncoder().encode(self) else { return nil }
+        return (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)).flatMap { $0 as? [String: Any] }
+    }
+}
+
+extension Decodable {
+    init(from: Any) throws {
+        let data = try JSONSerialization.data(withJSONObject: from, options: .prettyPrinted)
+        let decoder = JSONDecoder()
+        self = try decoder.decode(Self.self, from: data)
+    }
+}
