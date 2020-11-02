d4204758720d03a69dfced61ef0c4f3487ce3f09
Caldarie <michael_nguyen4@hotmail.com>
2020-11-02 23:21:15 +0900

    Replace future w/ stream and added multi runs

--- android/src/main/java/flutter/tflite_audio/TfliteAudioPlugin.java
+++ android/src/main/java/flutter/tflite_audio/TfliteAudioPlugin.java
@@ -46,9 +46,12 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry; //required for onRequestPermissionsResult
 import io.flutter.plugin.common.PluginRegistry.Registrar;
+import io.flutter.plugin.common.EventChannel;
+import io.flutter.plugin.common.EventChannel.EventSink;
+import io.flutter.plugin.common.EventChannel.StreamHandler;
 
 
-public class TfliteAudioPlugin implements MethodCallHandler, PluginRegistry.RequestPermissionsResultListener {
+public class TfliteAudioPlugin implements MethodCallHandler, StreamHandler, PluginRegistry.RequestPermissionsResultListener {
 
     //constants that control the behaviour of the recognition code and model settings
     //private static final int recordingLength = 16000;
@@ -67,42 +70,52 @@ public class TfliteAudioPlugin implements MethodCallHandler, PluginRegistry.Requ
     //ui elements
     private static final String LOG_TAG = "Tflite_audio";
     private static final int REQUEST_RECORD_AUDIO = 13;
+    private Handler handler = new Handler(Looper.getMainLooper());
 
     //working recording variables
+    AudioRecord record;
     short[] recordingBuffer;
+    short[] recordingBufferMax;
     int recordingOffset = 0;
     boolean shouldContinue = true;
     private Thread recordingThread;
-    private final Registrar registrar;
     private final ReentrantLock recordingBufferLock = new ReentrantLock();
 
     //working label variables
     private List<String> labels;
 
     //working recognition variables
+    // boolean shouldContinueRecognition = false;
+    boolean lastInferenceRun = false;
     private long lastProcessingTimeMs;
-    boolean shouldContinueRecognition = true;
     private Thread recognitionThread;
     private Interpreter tfLite;
     private LabelSmoothing labelSmoothing = null;
 
     //flutter
-    private Handler handler = new Handler(Looper.getMainLooper());
+    private final Registrar registrar;
     private HashMap arguments;
     private Result result;
+    private EventSink events;
 
-
-    public static void registerWith(Registrar registrar) {
-        final MethodChannel channel = new MethodChannel(registrar.messenger(), "tflite_audio");
-        TfliteAudioPlugin tfliteAudioPlugin = new TfliteAudioPlugin(registrar);
-        channel.setMethodCallHandler(tfliteAudioPlugin);
-        registrar.addRequestPermissionsResultListener(tfliteAudioPlugin);
-    }
 
     //initialises register variable with a constructor
     private TfliteAudioPlugin(Registrar registrar) {
         this.registrar = registrar;
     }
+
+    public static void registerWith(Registrar registrar) {
+        TfliteAudioPlugin tfliteAudioPlugin = new TfliteAudioPlugin(registrar);
+
+        final MethodChannel channel = new MethodChannel(registrar.messenger(), "tflite_audio");
+        channel.setMethodCallHandler(tfliteAudioPlugin);
+
+        final EventChannel eventChannel = new EventChannel(registrar.messenger(), "startAudioRecognition");
+        eventChannel.setStreamHandler(tfliteAudioPlugin);
+
+        registrar.addRequestPermissionsResultListener(tfliteAudioPlugin);
+    }
+
 
     @Override
     public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
@@ -117,14 +130,28 @@ public class TfliteAudioPlugin implements MethodCallHandler, PluginRegistry.Requ
                     result.error("failed to load model", e.getMessage(), e);
                 }
                 break;
-            case "startAudioRecognition":
-                checkPermissions();
+            case "stopAudioRecognition":
+                forceStopRecogniton();
                 break;
             default:
                 result.notImplemented();
                 break;
         }
     }
+
+
+    @Override
+    public void onListen(Object arguments, EventSink events) {
+        this.events = events;
+        this.arguments = (HashMap) arguments;
+        checkPermissions();
+    }
+
+    @Override
+    public void onCancel(Object arguments) {
+        this.events = null;
+    }
+
 
     private void loadModel() throws IOException {
         String model = arguments.get("model").toString();
@@ -229,11 +256,13 @@ public class TfliteAudioPlugin implements MethodCallHandler, PluginRegistry.Requ
                     "Permission has been declined. Please accept permissions in your settings"
             );
             //return false for hasPermission
-            Map <String, Object> finalResults = new HashMap();
+            Map<String, Object> finalResults = new HashMap();
             finalResults.put("recognitionResult", null);
             finalResults.put("inferenceTime", 0);
             finalResults.put("hasPermission", false);
-            result.success(finalResults);
+            if (events != null) {
+                events.success(finalResults);
+            }
         }
         return true;
     }
@@ -289,25 +318,30 @@ public class TfliteAudioPlugin implements MethodCallHandler, PluginRegistry.Requ
     private void record() {
         android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_AUDIO);
 
-        int bufferSize = (int) arguments.get("bufferSize");
-        int sampleRate = (int) arguments.get("sampleRate");
-        int recordingLength = (int) arguments.get("recordingLength");
+        final int bufferSize = (int) arguments.get("bufferSize");
+        final int sampleRate = (int) arguments.get("sampleRate");
+        final int recordingLength = (int) arguments.get("recordingLength");
+        final int numOfRecordings = (int) arguments.get("numOfRecordings");
+
+        int maxRecordingLength = recordingLength * numOfRecordings;
         short[] recordingFrameBuffer = new short[bufferSize / 2];
 
-        //initialize recordingBuffer
-        recordingBuffer = new short[recordingLength];
-
+        //Used to keep count 
+        recordingBuffer = new short[recordingLength]; //16000
+        recordingBufferMax = new short[maxRecordingLength]; //32000
+        int preRecordingCount = 0;
+        int recordingCount = recordingLength;
 
         // Estimate the buffer size we'll need for this device.
         // int bufferSize =
         //         AudioRecord.getMinBufferSize(
         //                 sampleRate, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
-        // if (bufferSize == AudioRecord.ERROR || bufferSize == AudioRecord.ERROR_BAD_VALUE) {
+        // if (bufferSize == AudioRecord.ERROR || bufferSize == Audi oRecord.ERROR_BAD_VALUE) {
         //     bufferSize = sampleRate * 2;
         // }
         // Log.v(LOG_TAG, "Buffer size: " + bufferSize);
 
-        AudioRecord record =
+        record =
                 new AudioRecord(
                         MediaRecorder.AudioSource.DEFAULT,
                         sampleRate,
@@ -326,34 +360,46 @@ public class TfliteAudioPlugin implements MethodCallHandler, PluginRegistry.Requ
 
 
         while (shouldContinue) {
+            //numberRead = represents the length of RecordingFrameBuffer (1280)
             int numberRead = record.read(recordingFrameBuffer, 0, recordingFrameBuffer.length);
-            int maxLength = recordingBuffer.length;
-            Log.v(LOG_TAG, "read: " + numberRead);
             recordingBufferLock.lock();
             try {
-                if (recordingOffset + numberRead < maxLength) {
-                    System.arraycopy(recordingFrameBuffer, 0, recordingBuffer, recordingOffset, numberRead);
-                } else {
-                    shouldContinue = false;
-                }
+                //Appends recordingFrameBuffer (length: 1280) to recordingBuffer (length: 16000)
+                System.arraycopy(recordingFrameBuffer, 0, recordingBufferMax, recordingOffset, numberRead);
+                //Used tp keep count
                 recordingOffset += numberRead;
-                Log.v(LOG_TAG, "recordingOffset: " + recordingOffset + "/" + maxLength);
+                Log.v(LOG_TAG, "recordingOffset: " + recordingOffset + "/" + maxRecordingLength);
+                //If 
+                if (recordingOffset + numberRead >= recordingCount) {
+                    Log.v(LOG_TAG, "Exceeded threshold");
+                    //inputs the first array
+                    System.arraycopy(recordingBufferMax, preRecordingCount, recordingBuffer, 0, recordingLength);
+                    startRecognition();
+                    if (recordingOffset + numberRead >= maxRecordingLength) {
+                        Log.v(LOG_TAG, "Stop recording..");
+                        stopRecording();
+                        //set variable for stream to close, after recognition is finished.
+                        lastInferenceRun = true;
+                    } else {
+                        Log.v(LOG_TAG, "looping...");
+                        recordingCount += recordingLength;
+                        preRecordingCount += recordingLength;
+                    }
+                }
             } finally {
                 recordingBufferLock.unlock();
 
             }
         }
-        record.stop();
-        record.release();
-        stopRecording();
-        startRecognition();
+        // stopRecording();
+        // startRecognition();
     }
 
     public synchronized void startRecognition() {
         if (recognitionThread != null) {
             return;
         }
-        shouldContinueRecognition = true;
+        // shouldContinueRecognition = true;
 
         recognitionThread =
                 new Thread(
@@ -378,7 +424,6 @@ public class TfliteAudioPlugin implements MethodCallHandler, PluginRegistry.Requ
         float[][] outputScores = new float[1][labels.size()];
         int[] sampleRateList = new int[]{sampleRate};
 
-       
 
         recordingBufferLock.lock();
         try {
@@ -404,9 +449,9 @@ public class TfliteAudioPlugin implements MethodCallHandler, PluginRegistry.Requ
         tfLite.runForMultipleInputsOutputs(inputArray, outputMap);
         lastProcessingTimeMs = new Date().getTime() - startTime;
 
-
-        Log.v(LOG_TAG, "OUTPUT======> " + Arrays.toString(outputScores[0]));
-        Log.v(LOG_TAG, Long.toString(lastProcessingTimeMs));
+        //debugging purposes
+        // Log.v(LOG_TAG, "OUTPUT======> " + Arrays.toString(outputScores[0]));
+        // Log.v(LOG_TAG, Long.toString(lastProcessingTimeMs));
 
         labelSmoothing =
                 new LabelSmoothing(
@@ -420,43 +465,82 @@ public class TfliteAudioPlugin implements MethodCallHandler, PluginRegistry.Requ
         long currentTime = System.currentTimeMillis();
         final LabelSmoothing.RecognitionResult recognitionResult =
                 labelSmoothing.processLatestResults(outputScores[0], currentTime);
-     
+
         //Map score and inference time
-        Map <String, Object> finalResults = new HashMap();
+        Map<String, Object> finalResults = new HashMap();
         finalResults.put("recognitionResult", recognitionResult.foundCommand);
         finalResults.put("inferenceTime", lastProcessingTimeMs);
         finalResults.put("hasPermission", true);
-        
-        //pass the map to flutter and stop recognition
+
         getResult(finalResults);
         stopRecognition();
     }
 
-    //When passing results, required to be sent via main thread.
-    public void getResult(Map <String, Object> recognitionResult) {
-        runOnUIThread(() -> {
-            Log.v(LOG_TAG, "result: " + recognitionResult.toString());
-            result.success(recognitionResult);
-        });
+    //passes map to from platform to flutter.
+    public void getResult(Map<String, Object> recognitionResult) {
 
+        //passing data from platform to flutter requires ui thread
+        runOnUIThread(() -> {
+            if (events != null) {
+                Log.v(LOG_TAG, "result: " + recognitionResult.toString());
+                events.success(recognitionResult);
+            }
+        });
     }
 
+
+
     public void stopRecognition() {
+        // if recognitThread hasn't been called. The function will break
         if (recognitionThread == null) {
             return;
         }
-        recognitionThread = null;
+
         Log.d(LOG_TAG, "Recognition stopped.");
+        recognitionThread = null;
+
+        //If last inference run is true, will close stream
+        if (lastInferenceRun == true) {
+            //passing data from platform to flutter requires ui thread
+            runOnUIThread(() -> {
+                if (events != null) {
+                    Log.d(LOG_TAG, "Recognition Stream stopped");
+                    events.endOfStream();
+                }
+            });
+            lastInferenceRun = false;
+        }
     }
 
     public void stopRecording() {
+        shouldContinue = false;
+
         if (recordingThread == null) {
             return;
         }
+
+        record.stop();
+        record.release();
+
         recordingOffset = 0; //reset recordingOffset
         recordingThread = null;//closes recording
         Log.d(LOG_TAG, "Recording stopped.");
     }
+
+    public void forceStopRecogniton() {
+
+        stopRecording();
+        stopRecognition();
+
+        //passing data from platform to flutter requires ui thread
+        runOnUIThread(() -> {
+            if (events != null) {
+                Log.d(LOG_TAG, "Recognition Stream stopped");
+                events.endOfStream();
+            }
+        });
+    }
+
 
     private void runOnUIThread(Runnable runnable) {
         if (Looper.getMainLooper() == Looper.myLooper())
