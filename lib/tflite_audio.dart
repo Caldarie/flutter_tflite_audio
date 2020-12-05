0d5113cd0c5034e792092a69e2efeacc1830bed3
Caldarie <michael_nguyen4@hotmail.com>
2020-12-05 18:59:42 +0900

    Documented public members

--- lib/tflite_audio.dart
+++ lib/tflite_audio.dart
@@ -2,17 +2,16 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
-// Class which manages the futures for the plugins
+/// Class which manages the future and stream for the plugins
 class TfliteAudio {
   static const MethodChannel _channel = MethodChannel('tflite_audio');
   static const EventChannel _eventChannel =
       EventChannel('startAudioRecognition');
 
-// Streams  event. Can return multiple map objects with the following values:
-//1. recognitionResult - string value
-//2. inferenceTime - int value
-//3. hasPermission - boolean which checks for permission
-//4. numOfRecording - number of inferences you want to make per recording.
+  /// [startAudioRecognition] returns map objects with the following values:
+  /// 1. String recognitionResult
+  /// 2. int inferenceTime
+  /// 3. bool hasPermission
   static Stream<Map<dynamic, dynamic>> startAudioRecognition(
       {String inputType,
       int sampleRate,
@@ -27,19 +26,20 @@ class TfliteAudio {
       'bufferSize': bufferSize,
       'numOfInferences': numOfInferences,
     });
+
+    ///cast the result of the stream a map object.
     return recognitionStream
         .cast<Map<dynamic, dynamic>>()
         .map((event) => Map<dynamic, dynamic>.from(event));
   }
 
-  //Future for loading your model and label
-//a larger num of threads will reduce inferenceTime.
+  ///call [stopAudioRecognition] to forcibly stop recording, recognition and
+  ///stream.
   static Future stopAudioRecognition() async {
     return _channel.invokeMethod('stopAudioRecognition');
   }
 
-//Future for loading your model and label
-//a larger num of threads will reduce inferenceTime.
+  ///initialize [loadModel] before calling any other streams and futures.
   static Future loadModel(
       {String model, String label, int numThreads, bool isAsset}) async {
     return _channel.invokeMethod(
