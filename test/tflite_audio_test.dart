b18540bb2213e8192073bde7fef15bd7ef3049f9
Caldarie <michael_nguyen4@hotmail.com>
2021-02-28 19:26:52 +0900

    Added test template

---
+++ test/tflite_audio_test.dart
@@ -0,0 +1,16 @@
+import 'package:tflite_audio/tflite_audio.dart';
+import 'package:flutter/services.dart';
+// import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('String.split() splits the string on the delimiter', () {
+    var string = 'foo,bar,baz';
+    expect(string.split(','), equals(['foo', 'bar', 'baz']));
+  });
+
+  test('String.trim() removes surrounding whitespace', () {
+    var string = '  foo ';
+    expect(string.trim(), equals('foo'));
+  });
+}
