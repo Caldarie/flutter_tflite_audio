0d60673c9e8c52391371541bcd931606c6b53c42
Caldarie <michael_nguyen4@hotmail.com>
2021-09-06 22:59:33 +0900

    Added tests

---
+++ test/tflite_audio_channel_test.dart
@@ -0,0 +1,106 @@
+/*
+check if argument passes
+https://github.com/flutter/plugins/blob/f93314bb3779ebb0151bc326a0e515ca5f46533c/packages/image_picker/image_picker_platform_interface/test/new_method_channel_image_picker_test.dart
+*/
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tflite_audio/tflite_audio.dart';
+import 'package:flutter/services.dart';
+
+final List<MethodCall> log = <MethodCall>[];
+const MethodChannel channel = MethodChannel('tflite_audio');
+// const MethodChannel eventChannel = MethodChannel('startAudioRecognition');
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  group('loadModel() test', () {
+    setUp(() async {
+      channel.setMockMethodCallHandler((methodCall) async {
+        log.add(methodCall);
+        return '';
+      });
+      log.clear();
+    });
+
+    tearDown(() {
+      channel.setMockMethodCallHandler(null);
+    });
+
+    test('passes optional and required arguments correctly', () async {
+      await TfliteAudio.loadModel(
+        model: 'assets/decoded_wav_model.tflite',
+        label: 'assets/decoded_wav_label.txt',
+      );
+
+      await TfliteAudio.loadModel(
+        model: 'assets/google_teach_machine_model.tflite',
+        label: 'assets/google_teach_machine_label.txt',
+        numThreads: 3,
+        isAsset: false,
+      );
+
+      expect(
+        log,
+        <Matcher>[
+          isMethodCall(
+            'loadModel',
+            arguments: <dynamic, dynamic>{
+              'model': 'assets/decoded_wav_model.tflite',
+              'label': 'assets/decoded_wav_label.txt',
+              'numThreads': 1,
+              'isAsset': true,
+            },
+          ),
+          isMethodCall(
+            'loadModel',
+            arguments: <dynamic, dynamic>{
+              'model': 'assets/google_teach_machine_model.tflite',
+              'label': 'assets/google_teach_machine_label.txt',
+              'numThreads': 3,
+              'isAsset': false,
+            },
+          ),
+        ],
+      );
+    });
+  });
+
+  // group('startAudioRecognition() test', () {
+  //   setUp(() async {
+  //     eventChannel.setMockMethodCallHandler((methodCall) async {
+  //       log.add(methodCall);
+  //       return '';
+  //     });
+  //     log.clear();
+  //   });
+
+  //   tearDown(() {
+  //     eventChannel.setMockMethodCallHandler(null);
+  //   });
+
+  //   test('passes optional and required arguments correctly', () async {
+  //     TfliteAudio.startAudioRecognition(
+  //         inputType: 'decodedWav',
+  //         sampleRate: 16000,
+  //         recordingLength: 16000,
+  //         bufferSize: 2000);
+
+  //     expect(
+  //       log,
+  //       <Matcher>[
+  //         isMethodCall(
+  //           'startAudioRecognition',
+  //           arguments: <dynamic, dynamic>{
+  //             'inputType': 'decodedWav',
+  //             'sampleRate': 16000,
+  //             'recordingLength': 16000,
+  //             'bufferSize': 2000,
+  //           },
+  //         ),
+  //       ],
+  //     );
+  //   });
+  // });
+}
---
+++ test/tflite_audio_stream_test.dart
@@ -0,0 +1,53 @@
+/*
+event streams
+https://github.com/befovy/fijkplayer/blob/58503f3f2591d41437bef61478de57b7527dff98/test/fijkplayer_test.dart
+https://github.com/BugsBunnyBR/plugin_gen.flutter/blob/master/examples/counter_stream_plugin/test/counter_stream_plugin_test.dart
+
+https://github.com/ZaraclaJ/audio_recorder/blob/master/test/audio_recorder_test.dart
+*/
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tflite_audio/tflite_audio.dart';
+import 'package:flutter/services.dart';
+
+const MethodChannel eventChannel = MethodChannel('startAudioRecognition');
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    eventChannel.setMockMethodCallHandler((methodCall) async {
+      await ServicesBinding.instance!.defaultBinaryMessenger
+          .handlePlatformMessage(
+              'startAudioRecognition',
+              const StandardMethodCodec()
+                  .encodeSuccessEnvelope(<dynamic, dynamic>{
+                'hasPermission': 'true',
+                'inferenceTime': '189',
+                'recognitionResult': 'no'
+              }),
+              (data) {});
+    });
+  });
+
+  tearDown(() {
+    eventChannel.setMockMethodCallHandler(null);
+  });
+
+  test('returns a map stream correctly', () async {
+    var value = TfliteAudio.startAudioRecognition(
+        inputType: 'decodedWav',
+        sampleRate: 16000,
+        recordingLength: 16000,
+        bufferSize: 2000);
+
+    value.listen(expectAsync1((event) {
+      expect(event, <dynamic, dynamic>{
+        'hasPermission': 'true',
+        'inferenceTime': '189',
+        'recognitionResult': 'no'
+      });
+    }));
+  });
+}
