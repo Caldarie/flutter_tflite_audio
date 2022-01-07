88ccd131ceeb1e26352c89b3cd93fb5e44cbc8fd
Caldarie <michael_nguyen4@hotmail.com>
2022-01-07 13:49:45 +0900

    Added - how to use audio player

--- android/src/main/java/flutter/tflite_audio/AudioPlayer.java
+++ android/src/main/java/flutter/tflite_audio/AudioPlayer.java
@@ -3,6 +3,18 @@ package flutter.tflite_audio;
 import android.media.AudioManager;
 import android.media.AudioTrack;
 
+/*
+!Debugging - How to use
+AudioPlayer audioPlayer;
+audioPlayer = new AudioPlayer(16000, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT);
+short[] out = new short[shortDataLength];
+    for (int i = 0; i < shortDataLength; i++) {
+        out[i] = shortBuffer.get(i);
+    }
+audioPlayer.play(out, 0, out.length);
+*/
+
+
 public class AudioPlayer {
 
     private AudioTrack audioTrack;
