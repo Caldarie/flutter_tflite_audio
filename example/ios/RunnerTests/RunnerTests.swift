ae4c29786c9fcf03f384686f0536b63854766752
Caldarie <michael_nguyen4@hotmail.com>
2022-03-09 12:37:51 +0900

    Added unit tests for iOS

--- example/ios/RunnerTests/RunnerTests.swift
+++ example/ios/RunnerTests/RunnerTests.swift
@@ -25,7 +25,7 @@ class RunnerTests: XCTestCase {
         var mockIsRecording = true
         
         let recordingData: RecordingData = RecordingData()
-        recordingData.setInputSize(audioLength: 3)
+        recordingData.setAudioLength(audioLength: 5)
         recordingData.setNumOfInferences(numOfInferences: 3)
         
         while(mockIsRecording){
@@ -63,8 +63,7 @@ class RunnerTests: XCTestCase {
              }
         }
         
-//        XCTAssertEqual(result, [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]) //4
-        XCTAssertEqual(result, [1, 2, 3, 4, 1, 2, 3, 4, 1]) //3
+        XCTAssertEqual(result, [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3])
     }
 
     func testAudioSplice(){
@@ -105,19 +104,19 @@ class RunnerTests: XCTestCase {
         XCTAssertEqual(result, array)
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    func testExample() throws {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//        // Any test you write for XCTest can be annotated as throws and async.
+//        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+//        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+//    }
+//
+//    func testPerformanceExample() throws {
+//        // This is an example of a performance test case.
+//        measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 
 }
