ffe4e25f84c2d26e754748b75c42486b4ff7039b
Caldarie <michael_nguyen4@hotmail.com>
2022-03-15 17:52:30 +0900

    Widget clean up and added comments

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,58 +5,65 @@ import 'package:tflite_audio/tflite_audio.dart';
 import 'package:flutter/services.dart';
 import 'dart:convert';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 ///This example showcases how to take advantage of all the futures and streams
 ///from the plugin.
 class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   final isRecording = ValueNotifier<bool>(false);
   Stream<Map<dynamic, dynamic>>? result;
 
-  // //!example values for decodedwav models
-  final String model = 'assets/decoded_wav_model.tflite';
-  final String label = 'assets/decoded_wav_label.txt';
-  final String audioDirectory = 'assets/sample_audio_16k_mono.wav';
-  final String inputType = 'decodedWav';
-  final int sampleRate = 16000;
-  final int bufferSize = 2000;
-  // final int audioLength = 16000;
+  ///example values for decodedwav models
+  // final String model = 'assets/decoded_wav_model.tflite';
+  // final String label = 'assets/decoded_wav_label.txt';
+  // final String audioDirectory = 'assets/sample_audio_16k_mono.wav';
+  // final String inputType = 'decodedWav';
+  // final int sampleRate = 16000;
+  // final int bufferSize = 2000;
+  // // final int audioLength = 16000;
 
-  //!example values for google's teachable machine model
-  // final String model = 'assets/google_teach_machine_model.tflite';
-  // final String label = 'assets/google_teach_machine_label.txt';
-  // final String inputType = 'rawAudio';
-  // final String audioDirectory = 'assets/sample_audio_44k_mono.wav';
-  // final int sampleRate = 44100;
-  // final int bufferSize = 22016;
-  // // final int audioLength = 44032;
+  ///example values for google's teachable machine model
+  final String model = 'assets/google_teach_machine_model.tflite';
+  final String label = 'assets/google_teach_machine_label.txt';
+  final String inputType = 'rawAudio';
+  final String audioDirectory = 'assets/sample_audio_44k_mono.wav';
+  final int sampleRate = 44100;
+  final int bufferSize = 22016;
+  // final int audioLength = 44032;
 
-  //!example values for MFCC, melspectrogram, spectrogram models
+  ///example values for MFCC, melspectrogram, spectrogram models
   // final String model = 'assets/spectrogram_model.tflite';
   // final String label = 'assets/spectrogram_label.txt';
   // final String inputType = 'spectrogram';
+
+  // final String model = 'assets/melspectrogram_model.tflite';
+  // final String label = 'assets/melspectrogram_label.txt';
   // final String inputType = 'melSpectrogram';
+
   // final String model = 'assets/mfcc_model.tflite';
   // final String label = 'assets/mfcc_label.txt';
   // final String inputType = 'mfcc';
+
   // final String audioDirectory = 'assets/sample_audio_16k_mono.wav';
   // final int sampleRate = 16000;
   // final int bufferSize = 2000;
-  // final int audioLength = 16000;
+  // // final int audioLength = 16000;
 
-  //!Optional parameters you can adjust to modify your input and output
+  ///Optional parameters you can adjust to modify your input and output
   final bool outputRawScores = false;
   final int numOfInferences = 5;
   final int numThreads = 1;
   final bool isAsset = true;
 
-  //!Adjust the values below when tuning model detection.
+  ///Adjust the values below when tuning model detection.
   final double detectionThreshold = 0.3;
   final int averageWindowDuration = 1000;
   final int minimumTimeBetweenSamples = 30;
@@ -68,58 +75,56 @@ class _MyAppState extends State<MyApp> {
     TfliteAudio.loadModel(
       // numThreads: this.numThreads,
       // isAsset: this.isAsset,
-      inputType: this.inputType,
-      outputRawScores: this.outputRawScores,
-      model: this.model,
-      label: this.label,
+      // outputRawScores: outputRawScores,
+      inputType: inputType,
+      model: model,
+      label: label,
     );
 
     //spectrogram parameters
     // TfliteAudio.setSpectrogramParameters(nFFT: 256, hopLength: 129);
 
     // mfcc parameters
-    // TfliteAudio.setSpectrogramParameters(nMFCC: 40, hopLength: 16384);
+    TfliteAudio.setSpectrogramParameters(nMFCC: 40, hopLength: 16384);
   }
 
-  /// get result by calling the stream startAudioRecognition
-  /// Uncomment the parameters below if you wish to adjust the values
   void getResult() {
-    //example for stored audio file recognition
+    ///example for stored audio file recognition
     result = TfliteAudio.startFileRecognition(
-      audioDirectory: this.audioDirectory,
-      sampleRate: this.sampleRate,
-      // audioLength: this.audioLength,
-      // detectionThreshold: this.detectionThreshold,
-      // averageWindowDuration: this.averageWindowDuration,
-      // minimumTimeBetweenSamples: this.minimumTimeBetweenSamples,
-      // suppressionTime: this.suppressionTime,
+      audioDirectory: audioDirectory,
+      sampleRate: sampleRate,
+      // audioLength: audioLength,
+      // detectionThreshold: detectionThreshold,
+      // averageWindowDuration: averageWindowDuration,
+      // minimumTimeBetweenSamples: minimumTimeBetweenSamples,
+      // suppressionTime: suppressionTime,
     );
 
-    //example for recording recognition
+    ///example for recording recognition
     // result = TfliteAudio.startAudioRecognition(
-    //   sampleRate: this.sampleRate,
-    //   bufferSize: this.bufferSize,
-    //   numOfInferences: this.numOfInferences,
-    //   // audioLength: this.audioLength,
-    //   // detectionThreshold: this.detectionThreshold,
-    //   // averageWindowDuration: this.averageWindowDuration,
-    //   // minimumTimeBetweenSamples: this.minimumTimeBetweenSamples,
-    //   // suppressionTime: this.suppressionTime,
+    //   sampleRate: sampleRate,
+    //   bufferSize: bufferSize,
+    //   numOfInferences: numOfInferences,
+    //   // audioLength: audioLength,
+    //   // detectionThreshold: detectionThreshold,
+    //   // averageWindowDuration: averageWindowDuration,
+    //   // minimumTimeBetweenSamples: minimumTimeBetweenSamples,
+    //   // suppressionTime: suppressionTime,
     // );
 
-    //Event returns a map. Keys are:
-    //event["recognitionResult"], event["hasPermission"], event["inferenceTime"]
+    ///Below returns a map of values. The keys are:
+    ///"recognitionResult", "hasPermission", "inferenceTime"
     result
         ?.listen((event) =>
             log("Recognition Result: " + event["recognitionResult"].toString()))
         .onDone(() => isRecording.value = false);
   }
 
-  //fetches the labels from the text file in assets
+  ///fetches the labels from the text file in assets
   Future<List<String>> fetchLabelList() async {
     List<String> _labelList = [];
     await rootBundle.loadString(this.label).then((q) {
-      for (String i in LineSplitter().convert(q)) {
+      for (String i in const LineSplitter().convert(q)) {
         _labelList.add(i);
       }
     });
@@ -138,12 +143,13 @@ class _MyAppState extends State<MyApp> {
             appBar: AppBar(
               title: const Text('Tflite-audio/speech'),
             ),
-            //Streambuilder for inference results
+
+            ///Streambuilder for inference results
             body: StreamBuilder<Map<dynamic, dynamic>>(
                 stream: result,
                 builder: (BuildContext context,
                     AsyncSnapshot<Map<dynamic, dynamic>> inferenceSnapshot) {
-                  //futurebuilder for getting the label list
+                  ///futurebuilder for getting the label list
                   return FutureBuilder(
                       future: fetchLabelList(),
                       builder: (BuildContext context,
@@ -154,19 +160,19 @@ class _MyAppState extends State<MyApp> {
                             if (labelSnapshot.hasData) {
                               return labelListWidget(labelSnapshot.data);
                             } else {
-                              return CircularProgressIndicator();
+                              return const CircularProgressIndicator();
                             }
-                            break;
                           case ConnectionState.waiting:
-                            //Widets will let the user know that its loading when waiting for results
+
+                            ///Widets will let the user know that its loading when waiting for results
                             return Stack(children: <Widget>[
                               Align(
                                   alignment: Alignment.bottomRight,
                                   child: inferenceTimeWidget('calculating..')),
                               labelListWidget(labelSnapshot.data),
                             ]);
-                            break;
-                          //Widgets will display the final results.
+
+                          ///Widgets will display the final results.
                           default:
                             return Stack(children: <Widget>[
                               Align(
@@ -184,36 +190,36 @@ class _MyAppState extends State<MyApp> {
                 }),
             floatingActionButtonLocation:
                 FloatingActionButtonLocation.centerFloat,
-            floatingActionButton: Container(
-                child: ValueListenableBuilder(
-                    valueListenable: isRecording,
-                    builder: (context, value, widget) {
-                      if (value == false) {
-                        return FloatingActionButton(
-                          onPressed: () {
-                            isRecording.value = true;
-                            setState(() {
-                              getResult();
-                            });
-                          },
-                          backgroundColor: Colors.blue,
-                          child: const Icon(Icons.mic),
-                        );
-                      } else {
-                        return FloatingActionButton(
-                          onPressed: () {
-                            log('Audio Recognition Stopped');
-                            //Press button again to cancel audio recognition
-                            TfliteAudio.stopAudioRecognition();
-                          },
-                          backgroundColor: Colors.red,
-                          child: const Icon(Icons.adjust),
-                        );
-                      }
-                    }))));
+            floatingActionButton: ValueListenableBuilder(
+                valueListenable: isRecording,
+                builder: (context, value, widget) {
+                  if (value == false) {
+                    return FloatingActionButton(
+                      onPressed: () {
+                        isRecording.value = true;
+                        setState(() {
+                          getResult();
+                        });
+                      },
+                      backgroundColor: Colors.blue,
+                      child: const Icon(Icons.mic),
+                    );
+                  } else {
+                    return FloatingActionButton(
+                      onPressed: () {
+                        log('Audio Recognition Stopped');
+
+                        ///Press button again to cancel audio recognition
+                        TfliteAudio.stopAudioRecognition();
+                      },
+                      backgroundColor: Colors.red,
+                      child: const Icon(Icons.adjust),
+                    );
+                  }
+                })));
   }
 
-  ///  If snapshot data matches the label, it will change colour
+  ///If snapshot data matches the label, it will change colour
   Widget labelListWidget(List<String>? labelList, [String? result]) {
     return Center(
         child: Column(
