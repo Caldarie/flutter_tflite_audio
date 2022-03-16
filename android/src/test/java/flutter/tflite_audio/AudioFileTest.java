92a319fe09cc731daf00fd76f6ade8e520be9256
Caldarie <michael_nguyen4@hotmail.com>
2022-03-16 12:13:41 +0900

    Updated unit test

--- android/src/test/java/flutter/tflite_audio/AudioFileTest.java
+++ android/src/test/java/flutter/tflite_audio/AudioFileTest.java
@@ -1,18 +1,13 @@
 package flutter.tflite_audio;
 
 import org.junit.Test;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+//import org.junit.Assert;
+//import static org.junit.Assert.assertFalse;
+//import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
 
 import java.util.ArrayList;
-//import java.util.Arrays;
-//import java.util.List;
-//import java.util.stream.Stream;
-//
-import android.util.Log;
-
-import androidx.core.content.res.TypedArrayUtils;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,15 +19,81 @@ public class AudioFileTest {
     private static final String LOG_TAG = "AudioFileTest";
 
     @Test
-    public void test_singleSplice() {
+    public void testSingleSplice() {
 
         short [] audioData = {1, 2, 3};
         int audioLength = 3;
         short [] expectedData = new short [] {1, 2, 3};
 
         List<Short> result = splice(audioData, audioLength, audioData.length);
-        assertEquals(convertToArray(result), expectedData);
+        assertArrayEquals(convertToArray(result), expectedData);
     }
+
+    @Test
+    public void testSingleSplice_lackData_noPad(){
+        short [] audioData = {1};
+        int audioLength = 3;
+        short [] expectedData = new short [] {};
+
+        List<Short> result = splice(audioData, audioLength, audioData.length);
+        assertArrayEquals(convertToArray(result), expectedData);
+    }
+
+    @Test
+    public void testSingleSplice_lackData_withPad(){
+        short [] audioData = {1, 2};
+        int audioLength = 3;
+        short [] expectedNoPad = audioData;
+        int expectedWithPadLength = 3;
+
+        List<Short> resultList = splice(audioData, audioLength, audioData.length);
+        short [] result = convertToArray(resultList);
+        short [] resultNoPad = Arrays.copyOfRange(result, 0, audioData.length);
+
+        System.out.println("singleSplice_lackData_withPad: " + resultList.toString());
+        assertArrayEquals(resultNoPad, expectedNoPad);
+        assertEquals(result.length, expectedWithPadLength);
+    }
+
+    @Test
+    public void testMultiSplice() {
+
+        short [] audioData = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int audioLength = 5;
+        short [] expectedData = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        List<Short> result = splice(audioData, audioLength, audioData.length);
+        assertArrayEquals(convertToArray(result), expectedData);
+    }
+
+    @Test
+    public void testMultiSplice_withExcess_noPadding() {
+
+        short [] audioData = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        int audioLength = 5;
+        short [] expectedData = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        List<Short> result = splice(audioData, audioLength, audioData.length);
+        assertArrayEquals(convertToArray(result), expectedData);
+    }
+
+    @Test
+    public void testMultiSplice_withExcess_withPadding() {
+
+        short [] audioData = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int audioLength = 6;
+        short [] expectedNoPad = audioData;
+        int expectedWithPadLength = 12;
+
+        List<Short> resultList = splice(audioData, audioLength, audioData.length);
+        short [] result = convertToArray(resultList);
+        short [] resultNoPad = Arrays.copyOfRange(result, 0, audioData.length);
+
+        System.out.println("multiSplice_lackData_withPad: " + resultList.toString());
+        assertArrayEquals(resultNoPad, expectedNoPad);
+        assertEquals(result.length, expectedWithPadLength);
+    }
+
 
     //https://stackoverflow.com/questions/60072435/how-to-convert-short-into-listshort-in-java-with-streams
     public List <Short> convertToList(short [] shortArray){
@@ -75,7 +136,7 @@ public class AudioFileTest {
                         .append(dataPoint);
                 break;
                 case "recognise":
-                    System.out.println("recognising");
+//                    System.out.println("recognising");
                     audioData
                         .append(dataPoint)
                         .displayInference()
@@ -85,7 +146,7 @@ public class AudioFileTest {
                         .reset();
                     break;
                 case "finalise":
-                    System.out.println("finalising");
+//                    System.out.println("finalising");
                     audioData
                         .append(dataPoint)
                         .displayInference()
@@ -95,7 +156,7 @@ public class AudioFileTest {
                     isSplicing = false;
                     break;
                 case "padAndFinalise":
-                    System.out.println("padding and finalising");
+//                    System.out.println("padding and finalising");
                     audioData
                         .append(dataPoint)
                         .padSilence(i)
